c04d5d78b69ea089f9fd6d1f42abb326c3daab99
ARizeDevs <developers@arize.io>
2020-12-14 20:41:26 +0100

    Critical fixes, updated the local host address

--- functions/src/client/web/config/api.ts
+++ functions/src/client/web/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'http://localhost:8080'
+export const baseURL = 'https://arizebackend-ikrcogluxq-uc.a.run.app'
 
 export default axios.create({
     baseURL
