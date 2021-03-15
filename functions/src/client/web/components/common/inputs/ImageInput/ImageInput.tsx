c0e57941e8419833bdd7d2618edc4549ba5e55b5
arash amigh <amigh.arash@gmail.com>
2021-03-15 15:43:55 +0330

    toggle bug

--- functions/src/client/web/components/common/inputs/ImageInput/ImageInput.tsx
+++ functions/src/client/web/components/common/inputs/ImageInput/ImageInput.tsx
@@ -52,7 +52,7 @@ const ImageInput = (props : IProps) => {
     return <div className={styles.root}>
             {/* <div style={{width:'100%',display:'flex',flexDirection:'row',justifyContent:'space-between'}}> */}
                 {/* <p style={{display:'flex',fontWeight:'bold',alignItems:'center'}} className={error || localError?styles.error:''}>{text}</p> */}
-                {toggle && setToggle?
+                {toggle !== undefined && setToggle !== undefined?
                     <Toggle  active={toggle} setActive={setToggle} text={text} />
                 :null}
             {/* </div> */}
