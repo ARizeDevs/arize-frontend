2be56e1c5cc7270eb25338c77bdfecfc507cb87a
ARizeDevs <developers@arize.io>
2021-03-22 16:52:34 +0100

    Upgrade modal base files added.

---
+++ functions/src/client/web/components/common/UpgradeModal/UpgradeModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Modal from 'react-modal';
+import { classNames } from 'react-select/src/utils';
+
+import CrossIcon from '../../../../assets/icons/cross.svg';
+
+import styles from './UpgradeModal.module.css'
+
+interface IProps {
+
+    isOpen : boolean,
+    onRequestClose : () => void,
+    text : string,
+    packageName: string,
+}
+
+const UpgradeModal = (props : IProps) => {
+    const {isOpen, onRequestClose, text, packageName} = props
+
+    return(
+        <Modal
+            isOpen = {isOpen}
+            onRequestClose={onRequestClose}
+            className={styles.upgradeModal}
+            overlayClassName={styles.overlay}
+            contentLabel="Upgrade Modal"
+        >
+            <div className={styles.column} style={{width:'100%',justifyContent:'center',alignItems:'center',height:'100%'}} >
+                <div style={{width:'100%',paddingRight:'30px',display:'flex',flexDirection:'row',justifyContent:'flex-end'}}>
+                    <div onClick={onRequestClose} style={{width:'30px',cursor:'pointer'}}>
+                        <CrossIcon />
+                    </div>
+                </div>
+                <br></br>
+                <p>{text}</p>
+            </div>
+        </Modal>
+    )
+
+}
+
+export default UpgradeModal;
