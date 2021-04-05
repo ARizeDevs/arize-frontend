0faebb1c1ad069bf4ee98ecdc3f40d6b6744ec02
arash amigh <amigh.arash@gmail.com>
2021-04-05 19:30:41 +0430

    hdr

--- functions/src/client/web/config/api.ts
+++ functions/src/client/web/config/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// export const baseURL = 'https://arize-backend-development-ikrcogluxq-uc.a.run.app'
+export const baseURL = 'https://arize-backend-development-ikrcogluxq-uc.a.run.app'
 // export const baseURL = 'https://arizebackend-ikrcogluxq-uc.a.run.app'
-export const baseURL = 'http://localhost:8080'
+// export const baseURL = 'http://localhost:8080'
 
 export default axios.create({ baseURL })
