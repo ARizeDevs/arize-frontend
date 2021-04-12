3e61303436cd8498537b9285b35f8e2157d4f70f
arash amigh <amigh.arash@gmail.com>
2021-04-12 14:47:42 +0430

    some changes

--- functions/src/client/web/config/api.ts
+++ functions/src/client/web/config/api.ts
@@ -1,13 +1,8 @@
 import axios from 'axios'
 
 
-<<<<<<< HEAD
-export const baseURL = 'https://arize-backend-development-ikrcogluxq-uc.a.run.app'
-// export const baseURL = 'https://arizebackend-ikrcogluxq-uc.a.run.app'
-=======
 // export const baseURL = 'https://arize-backend-development-ikrcogluxq-uc.a.run.app'
 export const baseURL = 'https://arizebackend-ikrcogluxq-uc.a.run.app'
->>>>>>> a906f5f0ece5e8f6a0f3e45eee91a1f289dcab83
 // export const baseURL = 'http://localhost:8080'
 
 export default axios.create({ baseURL })
